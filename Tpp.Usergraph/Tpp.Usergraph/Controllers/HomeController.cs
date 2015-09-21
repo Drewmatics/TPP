8835909d918f48ce9efd81e523d52cba92047917
Drew Leung <drew.leung@visioncritical.com>
2015-09-21 14:52:01 -0700

    Changing history API to use unverified balances

--- Tpp.Usergraph/Tpp.Usergraph/Controllers/HomeController.cs
+++ Tpp.Usergraph/Tpp.Usergraph/Controllers/HomeController.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using RestSharp;
 using Newtonsoft.Json;
+using RestSharp;
 using Tpp.Usergraph.Models;
 
 namespace Tpp.Usergraph.Controllers
 {
     public class HomeController : Controller
     {
+        private const string BaseUrl = "http://www.twitchplaysleaderboard.info/api";
         public ActionResult Index()
         {
             ViewBag.Message = "Use this to see a time vs. balance graph of your TPP earnings!";
@@ -21,15 +20,20 @@ namespace Tpp.Usergraph.Controllers
         [HttpPost]
         public ActionResult Graph(string username)
         {
-            var baseUrl = string.Format("http://www.twitchplaysleaderboard.info/api");
-            var restClient = new RestClient(baseUrl);
-            var restRequest = new RestRequest(string.Format("/user/history/{0}", username));
+            
+            var restClient = new RestClient(BaseUrl);
+            var queryParams = new List<string> {"unverified=true"};
+            var restRequest = new RestRequest(string.Format("/user/history/{0}?{1}", username, string.Join(",", queryParams)));
             
             var response = restClient.Get(restRequest);
-            var userHistory = JsonConvert.DeserializeObject<History>(response.Content);
+            var serializerSettings = new JsonSerializerSettings{ NullValueHandling = NullValueHandling.Ignore };
+            var userHistory = JsonConvert.DeserializeObject<History>(response.Content, serializerSettings);
             var matchData = userHistory.MatchData.FirstOrDefault();
+            if (matchData == null)
+                return new HttpStatusCodeResult(404, "User has no match history.");
             ViewBag.Username = matchData.Nickname;
-            ViewBag.Matches = matchData.Balance;
+            ViewBag.Count = matchData.Count;
+            ViewBag.Matches = matchData.Balance.Where(x => x.Amount > 0);
             return View();
         }
     }
--- Tpp.Usergraph/Tpp.Usergraph/Models/MatchData.cs
+++ Tpp.Usergraph/Tpp.Usergraph/Models/MatchData.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 
 namespace Tpp.Usergraph.Models
 {
     public class MatchData
     {
         public string Nickname { get; set; }
+        public int Count { get; set; }
         public List<Balance> Balance { get; set; }
     }
 }
